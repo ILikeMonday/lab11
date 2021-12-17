31003d7d9614af582fc8ec77cb326984402c6a57
ILikeMonday <worldrocker64@gmail.com>
2021-12-17 16:55:11 +0700

    all done

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt");
+    string num;
+    double sum;
+    double sum1;
+    int count;
+    while(getline(source,num)){
+        sum += atof(num.c_str());
+        sum1 += pow(atof(num.c_str()),2);
+        count++;
+    }
+    
+    cout <<"Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << "\n";
+    cout << "Standard deviation = " << sqrt(((sum1/count))-((sum/count)*(sum/count)));
+}
